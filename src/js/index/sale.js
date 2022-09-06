ee897611dd8b8b378bbcfc77f396654cef78e670
qianyabing <2534150597@qq.com>
2022-09-06 08:55:15 +0800

    20220906

--- src/js/index/sale.js
+++ src/js/index/sale.js
@@ -23,3 +23,27 @@ var mySwiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
 });
+document.querySelector(".goods_1").onclick = function () {
+    sessionStorage.setItem("id", 1);
+    window.open("describe.html");
+};
+document.querySelector(".goods_2").onclick = function () {
+    sessionStorage.setItem("id", 2);
+    window.open("describe.html");
+};
+document.querySelector(".goods_3").onclick = function () {
+    sessionStorage.setItem("id", 3);
+    window.open("describe.html");
+};
+document.querySelector(".goods_4").onclick = function () {
+    sessionStorage.setItem("id", 4);
+    window.open("describe.html");
+};
+document.querySelector(".goods_5").onclick = function () {
+    sessionStorage.setItem("id", 5);
+    window.open("describe.html");
+};
+document.querySelector(".goods_6").onclick = function () {
+    sessionStorage.setItem("id", 6);
+    window.open("describe.html");
+};
